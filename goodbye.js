8d47fbe9c19dbe4e00a894cf9961efc050a330eb
Madhu Murthy <msmurthy@msn.com>
2018-11-20 15:51:23 -0800

    assignment: Basic Node App Completed

---
+++ goodbye.js
@@ -0,0 +1,8 @@
+exports.goodbyeJapanese = function() {
+	console.log("Sayōnara");
+}
+
+exports.goodbyeSpanish = function() {
+	console.log("adiós");
+}
+
---
+++ main.js
@@ -0,0 +1,10 @@
+const greetings = require('./greeting.js');
+const goodbye = require('./goodbye.js');
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+
+goodbye.goodbyeJapanese();
+goodbye.goodbyeSpanish();
